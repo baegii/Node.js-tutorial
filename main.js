16ff83533577880607799244762c2cb6973a1080
Baegii <ogk12@yonsei.ac.kr>
2018-08-06 12:25:40 +0900

    비동기식 함수 실습

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-var http = require("http");
+var fs = require("fs");
 
-http.createServer(function(request, response){
-    /*
-        HTTP 헤더 전송
-        HTTP Status: 200 : OK
-        Content Type: text/plain
-    */
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+//비동기식 함수는 첫 매개변수로 error, 마지막 매개변수로 callback 함수를 호출한다.
+//fs.readFile()은 파일 내용을 다 읽고 출력한다.
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
 
-    //  Response Body를 "Hello World"로 설정
-    response.end("Hello World\n");
-}).listen(8081);
-
-console.log("Server running at http://127.0.0.1:8081");
+console.log("Program has ended");
+//결과는 맨 마지막 줄이 먼저 출력된다.
