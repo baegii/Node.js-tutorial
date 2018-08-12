4ad401ff79cf2534782508700d95723bd2bab52b
Baegii <ogk12@yonsei.ac.kr>
2018-08-12 23:43:52 +0900

    getUser api 추가

--- router/main.js
+++ router/main.js
@@ -12,5 +12,12 @@ module.exports = function(app, fs)
             console.log(data);
             res.end(data);
         });
-    })
+    });
+
+    app.get('/getUser/:username', function(req,res){
+        fs.readFile(__dirname + '/../data/user.json', 'utf8', function(err,data){
+            var users = JSON.parse(data);
+            res.json(users[req.params.username]);
+        });
+    });
 }
