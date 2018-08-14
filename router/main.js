23ad7baa7addb747077cab4feb14ec67437dd23d
Baegii <ogk12@yonsei.ac.kr>
2018-08-14 19:36:39 +0900

    Login/logout 기능 추가

--- router/main.js
+++ router/main.js
@@ -1,20 +1,23 @@
 module.exports = function(app, fs)
 {
     app.get('/', function(req,res){
-       res.render('index', {
+        var sess = req.session;
+
+        res.render('index', {
            title: "MY HOMEPAGE",
-           length: 5
-       })
+           length: 5,
+           name: sess.name,
+           username: sess.username
+        })
     });
 
-    // List 표시 api
+    // User List 표시 api
     app.get('/list', function(req,res){
         fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function(err,data){
             console.log(data);
             res.end(data);
         });
     });
-
     // User 표시 api
     app.get('/getUser/:username', function(req,res){
         fs.readFile(__dirname + '/../data/user.json', 'utf8', function(err,data){
@@ -22,7 +25,6 @@ module.exports = function(app, fs)
             res.json(users[req.params.username]);
         });
     });
-
     // User 추가 api
     app.post('/addUser/:username', function(req,res){
         var result = {};
@@ -56,7 +58,6 @@ module.exports = function(app, fs)
             })
         })
     })
-
     // User update api
     app.put('/updateUser/:username', function(req,res){
         var result = {};
@@ -90,7 +91,6 @@ module.exports = function(app, fs)
             })
         })
     })
-
     // User 삭제 api
     app.delete('/deleteUser/:username', function(req,res){
         var result = {};
@@ -112,7 +112,47 @@ module.exports = function(app, fs)
             fs.writeFile(__dirname + '/../data/user.json', JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
                 result = {'success' : 1};
                 res.json(result);
-            });
-        });
+            })
+        })
+    })
+
+    // Login api
+    app.get('/login/:username/:password', function(req,res){
+        var sess = req.session;
+
+        fs.readFile(__dirname + '/../data/user.json', 'utf8', function(err,data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+
+            if(!users[username]){
+                result['success'] = 0;
+                result['error'] = 'not found';
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password){
+                result['success'] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            }else{
+                result['success'] = 0;
+                result['error'] = 'password incorrect';
+                res.json(result);
+            }
+        })
+    })
+    // Logout api
+    app.get('/logout', function(req,res){
+        sess = req.session;
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err) console.log(err);
+                else res.redirect('/');
+            })
+        }else res.redirect('/');
     })
 }
